77b49af50bddafedd45b2e411a968f97a0338253
jimi-117 <sho.jimi117@gmail.com>
2026-01-19 14:12:09 +0100

    #01: fix eslint version has been updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  {
+    ignores: ["node_modules/", "dist/", "build/"]
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
